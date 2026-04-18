2570c20f0c308ea4aa55c6180117b65d9ebf5e0b
DESKTOP-AEBUROA\harry <ben.eric.zimmermann@gmail.com>
2026-04-18 15:01:14 +0200

    ADD enemy spawner to Room 1 and 2

--- Assets/Scripts/Enemy/RoomTrigger.cs
+++ Assets/Scripts/Enemy/RoomTrigger.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class RoomTrigger : MonoBehaviour
 {
-    RoomController roomController;
-    private void Awake()
-    {
-        roomController = GetComponentInParent<RoomController>();
-    }
+   [SerializeField] RoomController roomController;
+    //private void Awake()
+    //{
+    //    roomController = GetComponentInParent<RoomController>();
+    //}
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
